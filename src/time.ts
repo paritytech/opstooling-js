0fea49381d257458e48249f4088e2aaea1e4595e
cornholio <0@mcornholio.ru>
2022-09-14 18:03:49 +0200

    `until` helper + MaybePromise type

--- src/time.ts
+++ src/time.ts
@@ -1,5 +1,27 @@
+import { MaybePromise } from "./types"
+
 export const delay = (milliseconds: number) => {
   return new Promise<void>((resolve) => {
     setTimeout(resolve, milliseconds)
   })
+}
+
+export async function until(
+  cb: () => MaybePromise<boolean>,
+  interval: number,
+  maxRetries: number = Infinity,
+  timeoutMessage?: string,
+): Promise<void> {
+  let retryCount: number = 0
+
+  while (retryCount < maxRetries) {
+    const res = await cb()
+    if (res) {
+      return
+    }
+    await delay(interval)
+    retryCount++
+  }
+
+  throw new Error(timeoutMessage ?? "Maximun retry count reached")
 }

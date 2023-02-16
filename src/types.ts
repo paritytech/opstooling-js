7c022e58da3bc7e0b43d14176724210d5057090a
Przemek Rzad <przemek@parity.io>
2023-02-16 14:53:36 +0100

    Deprecate the Ok/Err pattern (#36)

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,43 @@
+const deprecateOkErr = () =>
+  console.warn(
+    "The usage of the Ok/Err pattern is deprecated because of the danger of mismatched 'instanceof' types in node_modules. " +
+      "Please use the regular try-catch pattern.",
+  );
+
+/**
+ * @deprecated
+ * The usage of the Ok/Err pattern is deprecated because of the danger of mismatched 'instanceof' types in node_modules.
+ * Please use the regular try-catch pattern.
+ */
 export class Ok<T> {
-  constructor(public value: T) {}
+  constructor(public value: T) {
+    deprecateOkErr();
+  }
 }
 
+/**
+ * @deprecated
+ * The usage of the Ok/Err pattern is deprecated because of the danger of mismatched 'instanceof' types in node_modules.
+ * Please use the regular try-catch pattern.
+ */
 export const ok = <T>(value: T): Ok<T> => new Ok(value);
 
+/**
+ * @deprecated
+ * The usage of the Ok/Err pattern is deprecated because of the danger of mismatched 'instanceof' types in node_modules.
+ * Please use the regular try-catch pattern.
+ */
 export class Err<T> {
-  constructor(public value: T) {}
+  constructor(public value: T) {
+    deprecateOkErr();
+  }
 }
 
+/**
+ * @deprecated
+ * The usage of the Ok/Err pattern is deprecated because of the danger of mismatched 'instanceof' types in node_modules.
+ * Please use the regular try-catch pattern.
+ */
 export const err = <T>(value: T): Err<T> => new Err(value);
 
 type KeysOfType<T, U> = { [K in keyof T]: T[K] extends U ? K : never }[keyof T];

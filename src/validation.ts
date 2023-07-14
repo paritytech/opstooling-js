b639804de07017591a5913115ee3091725413466
Javier Bullrich <javier.bullrich@parity.io>
2023-07-14 15:53:07 +0200

    Added docs generation (#46)
    Closes #45 
    
    Added automatic generation of docs using `typedoc`.
    
    Also added docs to `validate` method.

--- src/validation.ts
+++ src/validation.ts
@@ -5,6 +5,21 @@ type Options = {
   message?: string;
 };
 
+/**
+ * Validates an object against to a JOI schema
+ *
+ * @example
+ * import { validate } from "@eng-automation/js";
+ * import { configSchema } from "./utils";
+ *
+ * const myConfig: unknown = JSON.parse(jsonString);
+ * const config: ConfigurationFile validate<ConfigurationFile>(config, configSchema, { message: "Configuration file is invalid" });
+ *
+ * @param item the javascript object to be parsed.
+ * @param schema the [JOI.schema object]({@link https://joi.dev/api/})
+ * @param options an object with a message value. See example.
+ * @returns the objected converted to the given type
+ */
 export function validate<T>(item: unknown, schema: Joi.Schema, options: Options = {}): T {
   const joiOpts = { allowUnknown: true, abortEarly: false, convert: true };
 
